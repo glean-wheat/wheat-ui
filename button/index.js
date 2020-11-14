6158e5fb57e2659ae231ed2842d1676c880030eb
wugaoliang <wugaoliang@xiaomi.com>
2020-11-15 06:54:23 +0800

    init

---
+++ button/index.js
@@ -0,0 +1,70 @@
+const template = document.createElement('template')
+
+template.innerHTML = `
+  <style>
+    .wheat-button-container {
+      padding: 8px;
+    }
+    .wheat-button {
+        position: relative;
+        display: inline-block;
+        box-sizing: border-box;
+        margin: 0;
+        height: 32px;
+        padding: 4px 15px;
+        border: 1px solid transparent;
+        border-radius: 2px;
+        font-size: 14px;
+        font-weight: 400;
+        line-height: 22px;
+        text-align: center;
+        text-decoration: none;
+        vertical-align: middle;
+        white-space: nowrap;
+        touch-action: manipulation;
+        cursor: pointer;
+        transition: background-color 0.3s ease-in, border-color 0.3s ease-in,
+          color 0.3s ease-in, -webkit-box-shadow 0.15s ease-in;
+        transition: background-color 0.3s ease-in, border-color 0.3s ease-in,
+          box-shadow 0.15s ease-in, color 0.3s ease-in;
+        transition: background-color 0.3s ease-in, border-color 0.3s ease-in,
+          box-shadow 0.15s ease-in, color 0.3s ease-in,
+          -webkit-box-shadow 0.15s ease-in;
+          outline: none;
+    }
+    .cancel{
+        background-color: #f0f5fe;
+        border-color: #b4cffb;
+    }
+    .confirm {
+        background-color: #4387f4;
+        color: #fff;
+    }
+  </style>
+ 
+  <div class="wheat-button-container">
+    <button class='wheat-button'>Label</button>
+  </div>
+`
+
+class Button extends HTMLElement {
+  constructor() {
+    super()
+
+    this._shadowRoot = this.attachShadow({ mode: 'open' })
+    this._shadowRoot.appendChild(template.content.cloneNode(true))
+  }
+  connectedCallback() {
+    this.$button = this._shadowRoot.querySelector('.wheat-button')
+    const type = this.getAttribute('type')
+    this.$button.classList.add(type)
+    this.$button.innerHTML = this.innerHTML
+    this.$button.addEventListener('click', () => {
+      new CustomEvent('click', {
+        detail: { visiable: false }
+      })
+    })
+  }
+}
+
+window.customElements.define('wheat-button', Button)
