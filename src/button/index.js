a9d7bd785fe7d527312ef403df800884c2880b9e
wugaoliang <wugaoliang@xiaomi.com>
2020-11-24 22:48:51 +0800

    fix: btn

--- src/button/index.js
+++ src/button/index.js
@@ -428,10 +428,6 @@ class WheatButton extends HTMLElement {
         : template.content.cloneNode(true)
     )
   }
-  renderShadowDom() {
-    this._shadowRoot = this.attachShadow({ mode: 'open' })
-    this._shadowRoot.appendChild(WheatModaltemplate.content.cloneNode(true))
-  }
 }
 
 window.customElements.define('wheat-button', WheatButton)
