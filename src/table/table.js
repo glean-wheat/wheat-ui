051ea357d0923fb44003ab2a335391d4e03e8af9
wugaoliang <wugaoliang@baidu.com>
2023-03-13 21:14:34 +0800

    feat: VirtualTable setTable v1.1.18

--- src/table/table.js
+++ src/table/table.js
@@ -1,12 +1,8 @@
-import React, {useState, useRef, useEffect} from 'react'
+import React, {useState} from 'react'
 import ReactDOM from 'react-dom/client'
 import * as antdComponent from 'antd'
 import * as styleContent from './table.scss'
 import {cloneDeep} from 'lodash';
-import classNames from 'classnames';
-import { VariableSizeGrid as Grid } from 'react-window'
-import ResizeObserver from 'rc-resize-observer';
-
 const template = document.createElement('template')
 const prefix = 'wheat-table'
 
@@ -41,7 +37,7 @@ class WheatTable extends HTMLElement {
   }
 
   setDataSource(data){
-    this.tableDataSource = data.concat();
+    this.tableDataSource = data;
     this.renderReactElement();
   }
   parseRender (renderInfo, text, row) {
@@ -87,123 +83,12 @@ class WheatTable extends HTMLElement {
     const data = this.tableDataSource;
     const Table = antdComponent['Table']
     this.root.render((() => {
-      const VirtualTable = () => {
-        const { scroll } = this.tableProps;
-        const columns = this.columns;
-        const [tableWidth, setTableWidth] = useState(0);
+      const Content = () => {
         const [tableData, setTableData] = useState(data);
         this.setTableData = setTableData;
-        const widthColumnCount = columns.filter(({ width }) => !width).length;
-        const mergedColumns = columns.map((column) => {
-          if (column.width) {
-            return column;
-          }
-          return {
-            ...column,
-            width: Math.floor(tableWidth / widthColumnCount),
-          };
-        });
-        const gridRef = useRef();
-        const [connectObject] = useState(() => {
-          const obj = {};
-          Object.defineProperty(obj, 'scrollLeft', {
-            get: () => {
-              if (gridRef.current) {
-                return gridRef.current?.state?.scrollLeft;
-              }
-              return null;
-            },
-            set: (scrollLeft) => {
-              if (gridRef.current) {
-                gridRef.current.scrollTo({
-                  scrollLeft,
-                });
-              }
-            },
-          });
-          return obj;
-        });
-        const resetVirtualGrid = () => {
-          gridRef.current?.resetAfterIndices({
-            columnIndex: 0,
-            shouldForceUpdate: true,
-          });
-        };
-        useEffect(() => resetVirtualGrid, [tableWidth]);
-        const renderVirtualList = (rawData, { scrollbarSize, ref, onScroll }) => {
-          ref.current = connectObject;
-          const totalHeight = rawData.length * 48;
-          return (
-            <Grid
-              ref={gridRef}
-              className="virtual-grid"
-              columnCount={mergedColumns.length}
-              columnWidth={(index) => {
-                const { width } = mergedColumns[index];
-                return totalHeight > scroll.y && index === mergedColumns.length - 1
-                  ? width - scrollbarSize - 1
-                  : width;
-              }}
-              height={scroll.y}
-              rowCount={rawData.length}
-              rowHeight={() => 48}
-              width={tableWidth}
-              onScroll={({ scrollLeft }) => {
-                onScroll({
-                  scrollLeft,
-                });
-              }}
-            >
-              {({ columnIndex, rowIndex, style }) =>{
-                const text = rawData[rowIndex][mergedColumns[columnIndex].dataIndex]
-                const {children} = mergedColumns[columnIndex]
-                if(children && children.length) {
-                  return (
-                    <div className='antd-cell-wrapper' style={{...style, display: 'flex'}}>
-                      {children.map((item) => {
-                        const childrenText = rawData[rowIndex][item.dataIndex]
-                        return <div className='virtual-table-cell' style={{width: item.width, align: item.align || 'left'}}>{item.render ? item.render(childrenText, rawData[rowIndex], rowIndex) : childrenText}</div>
-                      })}
-                    </div>
-                  )
-                }
-                return (
-                  <div
-                    className={classNames('virtual-table-cell antd-cell-wrapper', {
-                      'virtual-table-cell-last': columnIndex === mergedColumns.length - 1,
-                    })}
-                    style={{
-                      ...style, 
-                      align: mergedColumns[columnIndex].align || 'left'
-                    }}
-                  >
-                    {mergedColumns[columnIndex].render ? mergedColumns[columnIndex].render(text, rawData[rowIndex], rowIndex) : text}
-                  </div>
-                )
-              }}
-            </Grid>
-          );
-        };
-        return (
-          <ResizeObserver
-            onResize={({ width }) => {
-              setTableWidth(width);
-            }}
-          >
-            <Table
-              {...this.tableProps}
-              className="virtual-table"
-              columns={mergedColumns}
-              pagination={false}
-              dataSource={tableData}
-              components={{
-                body: renderVirtualList,
-              }}
-            />
-          </ResizeObserver>
-        );
-      };
-      return <VirtualTable columns={this.columns} dataSource={data} {...this.tableProps} />
+        return <Table columns={this.columns} dataSource={tableData} {...this.tableProps} />
+      }
+      return <Content />
     })());
   }
 
